0bb942e1475007210f0e170c13cf3b892be8139b
rohankhatik <rc7354463@gmail.com>
2026-01-08 16:29:04 +0530

    Added Grade Calculator

---
+++ Program2.py
@@ -0,0 +1,13 @@
+name = input("Enter student name: ")
+marks = int(input("Enter marks: "))
+
+if marks >= 90:
+    grade = "A"
+elif marks >= 75:
+    grade = "B"
+elif marks >= 60:
+    grade = "C"
+else:
+    grade = "Fail"
+
+print(name, "Grade:", grade)
